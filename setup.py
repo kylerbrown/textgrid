77cb54c58de6aa50c0564972a5bf532506c6b3e0
Alexandre Defossez <alexandre.defossez@gmail.com>
2020-10-22 13:37:34 +0200

    fixing install from pip

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='textgrid',
       author='Kyler Brown',
       author_email='kylerjbrown@gmail.com',
       license='MIT',
-      py_modules="textgrid",
+      py_modules=["textgrid"],
       entry_points= {
           'console_scripts' : [
               'textgrid2csv = textgrid:textgrid2csv',
